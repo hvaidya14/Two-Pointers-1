643509ab8427c04522623c681561cf5c585cea10
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-13 21:20:07 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -40,3 +40,51 @@ class Solution {
     }
 
 }
+
+
+//SPACE COMPLEXITY:O(1)
+//TIME COMPLEXITY:O(N^2)
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i=0;i<nums.length;i++) {
+            int pleft = i+1;
+            int pright = nums.length-1;
+            if (nums[i] > 0){
+                break;
+            }
+            if (i!=0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            while(pleft < pright) {
+                int sum = nums[pleft] +nums[i] + nums[pright];
+                if(sum == 0) {
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[pleft]);
+                    temp.add(nums[pright]);
+                    result.add(temp);
+                    pleft++;
+                    pright--;
+                    while(pleft < pright && nums[pleft-1] == nums[pleft]){
+                        pleft++;
+                    }
+                    while (pright > pleft && nums[pright] == nums[pright+1]){
+                        pright--;
+                    }
+                } else if (sum > 0) {
+                    pright--;
+                } else if(sum < 0){
+                    pleft++;
+                }
+
+            }
+        }
+
+        return result;
+    }
+}
