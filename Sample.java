1c057fd09c3edf6c98489fca7f90b5ca74cc4bac
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-13 20:10:04 -0800

    COMMIT-1 Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,38 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+//SPACE COMPLEXITY:O(1)
+//TIME COMPLEXITY:O(N)
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        if(nums.length == 1) {
+            return;
+        }
+        int pleft =0;
+        int curr = 0;
+        int pright=nums.length-1;
+
+        while (curr<=pright) {
+            if (nums[curr] == 0) {
+                int temp = nums[curr];
+                nums[curr]=nums[pleft];
+                nums[pleft] = temp;
+                pleft++;
+                curr++;
+            } else if(nums[curr] == 2) {
+                int temp = nums[pright];
+                nums[pright] = nums[curr];
+                nums[curr] = temp;
+                pright--;
+            } else {
+                curr++;
+            }
+        }
+    }
+
+}
