7755d0afff17ad2cacdf4ac24f6221ab39976401
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-15 10:24:23 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,101 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+
+        int pleft = 0;
+        int pright = height.length-1;
+        int max=0;
+
+        while (pleft < pright) {
+            int area = Math.min(height[pleft], height[pright]) * (pright-pleft);
+            max = Math.max(area, max);
+            System.out.println(max);
+            if (height[pleft] <= height[pright]) {
+                pleft++;
+            } else if (height[pright] < height[pleft]) {
+                pright--;
+            }
+        }
+
+        return max;
+    }
+}
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i=0;i<nums.length;i++) {
+            int pleft = i+1;
+            int pright = nums.length-1;
+            if (nums[i] > 0){
+                break;
+            }
+            if (i!=0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            while(pleft < pright) {
+                int sum = nums[pleft] +nums[i] + nums[pright];
+                if(sum == 0) {
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[pleft]);
+                    temp.add(nums[pright]);
+                    result.add(temp);
+                    pleft++;
+                    pright--;
+                    while(pleft < pright && nums[pleft-1] == nums[pleft]){
+                        pleft++;
+                    }
+                    while (pright > pleft && nums[pright] == nums[pright+1]){
+                        pright--;
+                    }
+                } else if (sum > 0) {
+                    pright--;
+                } else if(sum < 0){
+                    pleft++;
+                }
+
+            }
+        }
+
+        return result;
+    }
+}
+
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        if(nums.length == 1) {
+            return;
+        }
+        int pleft =0;
+        int curr = 0;
+        int pright=nums.length-1;
+
+        while (curr<=pright) {
+            if (nums[curr] == 0) {
+                int temp = nums[curr];
+                nums[curr]=nums[pleft];
+                nums[pleft] = temp;
+                pleft++;
+                curr++;
+            } else if(nums[curr] == 2) {
+                int temp = nums[pright];
+                nums[pright] = nums[curr];
+                nums[curr] = temp;
+                pright--;
+            } else {
+                curr++;
+            }
+        }
+    }
+
+}
